479cf4c8c99776bd4b6fdaaa7457fb1fecfbc9d2
Gary Wu <garywu3291@gmail.com>
2023-05-22 15:54:16 +0800

    script for slideshow
    maybe make it automatic, not sure, have to test implementation first with images.

---
+++ slideChange.js
@@ -0,0 +1,39 @@
+let Index = 1;
+Slides(Index);
+
+function nextSlide(n){
+    Slides(Index += n);
+}
+
+function previousSlide(n){
+    Slides(Index -= n);
+}
+
+function Slides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElement("caption");
+    //Checking max
+    if(n > slides.length)
+    {
+        Index = 1;
+    }
+    //Making sure its always above 1
+    if(n < 1)
+    {
+        index = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+
+    for(i = 0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[Index -1].style.display = "block";
+    dots[Index - 1].className += " active";
+}
